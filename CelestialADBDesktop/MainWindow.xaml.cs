5429d8f3171b520d0da61a3564f4f4e8923d038d
Mark <markh@rris.com.au>
2017-01-25 21:22:45 -0700

    Add project files.

---
+++ CelestialADBDesktop/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using CelestialADBDesktop.ViewModel;
+
+namespace CelestialADBDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the MainWindow class.
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += (s, e) => ViewModelLocator.Cleanup();
+        }
+    }
+}

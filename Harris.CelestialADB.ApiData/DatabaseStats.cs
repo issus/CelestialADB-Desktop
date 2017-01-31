3d45599a3555d6cd251f53e245dcb02ac71b6285
Mark <markh@rris.com.au>
2017-01-31 00:20:59 -0700

    Added DatabaseStats class

---
+++ Harris.CelestialADB.ApiData/DatabaseStats.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harris.CelestialADB.ApiData
+{
+    public class DatabaseStats
+    {
+        public int ComponentCount { get; set; }
+        public int FootprintCount { get; set; }
+    }
+}

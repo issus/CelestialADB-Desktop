aca3c3dee312f851a380e0e3188be12aea5b49bc
Mark <markh@rris.com.au>
2017-01-31 02:07:45 -0700

    Changed ApiData to normal .net lib from portable. Added token class.

---
+++ Harris.CelestialADB.ApiData/AuthToken.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harris.CelestialADB.ApiData
+{
+    public class AuthToken
+    {
+        public string access_token { get; set; }
+        public string token_type { get; set; }
+        public int expires_in { get; set; }
+        public string userName { get; set; }
+        [DeserializeAs(Name = ".issued")]
+        public string issued { get; set; }
+        [DeserializeAs(Name = ".expires")]
+        public string expires { get; set; }
+    }
+}
--- Harris.CelestialADB.ApiData/Properties/AssemblyInfo.cs
+++ Harris.CelestialADB.ApiData/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -14,7 +13,14 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b1ef1595-9906-431f-b1a2-51d6e0751f3d")]
 
 // Version information for an assembly consists of the following four values:
 //
